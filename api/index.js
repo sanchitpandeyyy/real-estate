89b66f6379c97a6912db8cdf0d7c670a00debe0e
Your Name <Sanchit Pandey>
2024-06-06 02:10:05 +0545

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
-import express from "express"
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express()
+mongoose.connect(process.env.MONGO)
 
-app.get('/',(req,res)=>{
-    res.send("Helloworld")
-})
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Helloworld");
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+    console.log('Server is running on port 3000');
+});

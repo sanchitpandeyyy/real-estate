44a486f036f6fcf6df7783a4cf872051129d83c6
Your Name <Sanchit Pandey>
2024-06-07 22:57:39 +0545

    create signin api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const auth = async (req, res, next) => {
+export const signup = async (req, res, next) => {
 
     const { username, email, password } = req.body;
 
@@ -15,12 +17,34 @@ export const auth = async (req, res, next) => {
 
         res.status(201).json("User Created Successfully")
     } catch (error) {
-      
-      //error form middleware
+
+        //error form middleware
         next(error)
     }
 
 }
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not Found'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) return next(errorHandler(401, 'Invalid Password'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_Secret)
+        const {password: pass, ...rest}= validUser._doc
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 
 //logics are written in controllers
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { auth } from "../controllers/auth.controllers.js"
+import { signup } from "../controllers/auth.controllers.js"
+import { signin } from "../controllers/auth.controllers.js"
 
 const router = express.Router();
 
-router.post('/signup', auth)
+router.post('/signup', signup)
+router.post('/signin', signin)
 
 export default router;
